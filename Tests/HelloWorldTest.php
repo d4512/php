2ebb48d75571802fffbed274e930c8ead9ac02ca
Subrata <ssur.linux@gmail.com>
2016-06-29 14:43:38 +0530

    Make it pass again

--- Tests/HelloWorldTest.php
+++ Tests/HelloWorldTest.php
@@ -41,7 +41,7 @@ class HelloWorldTest extends PHPUnit_Framework_TestCase
 
         $helloWorld->hello('Bar');
 
-        $this->assertEquals('bar', $helloWorld->what());
+        $this->assertEquals('Bar', $helloWorld->what());
     }
 }
 
